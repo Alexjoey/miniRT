14cb58ae1d354046562d3421476b3ced768d9ba1
Alex Mylle <alexm@live.be>
2025-01-16 22:41:30 +0100

    add planes and very very basic shading based on distance

--- src/camera_rays/camera_rays.c
+++ src/camera_rays/camera_rays.c
@@ -68,60 +68,75 @@ void	ft_swapf(float *nptr1, float *nptr2)
 	*nptr2 = tmp;
 }
 
-bool	inter_sphere(t_vector *c_pos, t_vector *c_dir, t_sphere *sphere, float *t)
+bool	inter_sphere(t_vector *c_pos, t_vector *c_dir, t_sphere *sph, float *t)
 {
 	t_vector	l;
-	float		t0;
-	float		t1;
+	float		td[2];
 	float		tca;
 	float		thc;
 	float		d2;
 
-	l = subtract_vector(sphere->pos, *c_pos);
+	l = subtract_vector(sph->pos, *c_pos);
 	tca = dot_product(l, *c_dir);
 	if (tca < 0)
 		return (false);
 	d2 = dot_product(l, l) - tca * tca;
-	if (d2 > sphere->rad_sq)
+	if (d2 > sph->rad_sq)
 		return (false);
-	thc = sqrtf(sphere->rad_sq - d2);
-	t0 = tca - thc;
-	t1 = tca + thc;
-	if (t0 > t1)
-		ft_swapf(&t0, &t1);
-	if (t0 < 0)
-		t0 = t1;
-	if (t0 < 0)
+	thc = sqrtf(sph->rad_sq - d2);
+	td[0] = tca - thc;
+	td[1] = tca + thc;
+	if (td[0] > td[1])
+		ft_swapf(&td[0], &td[1]);
+	if (td[0] < 0)
+		td[0] = td[1];
+	if (td[0] < 0)
 		return (false);
-	*t = t0;
+	*t = td[0];
 	return (true);
+}
+
+//can maybe cache pl->pos and c_pos subtraction result
+bool	inter_plane(t_vector *c_pos, t_vector *c_dir, t_plane *pl, float *t)
+{
+	float	denominator;
+
+	denominator = dot_product(pl->direction, *c_dir);
+	if (denominator > 1e-6)
+	{
+		*t = dot_product(subtract_vector(pl->pos, *c_pos), pl->direction) / denominator;
+		return (*t >= 0);
+	}
+	return (false);
 }
 
 bool	intersect(t_vector *c_pos, t_vector *c_dir, t_shape *shape, float *t)
 {
 	if (shape->type == SPHERE)
 		return (inter_sphere(c_pos, c_dir, &shape->shape.sphere, t));
-/*	if (shape->type == PLANE)
-		return (intersect_plane());
-	if (shape->type == CYLINDER)
+	if (shape->type == PLANE)
+		return (inter_plane(c_pos, c_dir, &shape->shape.plane, t));
+/*	if (shape->type == CYLINDER)
 		return (intersect_cylinder());*/
 	return (false);
 }
 
-t_color	trace_ray(t_vector *camera_pos, t_vector *camera_dir, t_rt *obj)
+int	trace_ray(t_vector *camera_pos, t_vector *camera_dir, t_rt *obj)
 {
-//	t_color		color;
-//	t_vector	p_hit;
-//	t_vector	n_hit;
-//	float		d;
 	float		min_d;
 	t_shape		*shape;
 	t_shape		*ptr;
 	float		t;
+	float		percent;
+//	t_color		color;
+//	t_vector	p_hit;
+//	t_vector	n_hit;
+//	float		d;
 
 	ptr = obj->shapes;
 	shape = NULL;
 	min_d = INFINITY;
+	t = 0;
 	while (ptr)
 	{
 		if (intersect(camera_pos, camera_dir, ptr, &t) && t < min_d)
@@ -131,17 +146,36 @@ t_color	trace_ray(t_vector *camera_pos, t_vector *camera_dir, t_rt *obj)
 		}
 		ptr = ptr->next;
 	}
+	
 	if (shape)
-		return (shape->shape.sphere.color);
-	return (set_color(0, 0, 0));
+	{
+		percent = min_d * 0.003;
+		if (percent > 1.0)
+			percent = 1.0;
+		if (percent < 0.2)
+			percent = 0.2;
+		if (shape->type == SPHERE)
+			return (interpol_color(convert_color(shape->shape.sphere.color), 0, percent));
+		if (shape->type == PLANE)
+			return (interpol_color(convert_color(shape->shape.plane.color), 0, percent));
+		/*if (shape->type == CYLINDER)
+			return (interpol_color(convert_color(shape->shape.cylinder.color), 0, percent));*/
+	}
+	return (0);
 }
+
+#include <time.h>
 
 void	render(t_rt *obj)
 {
 	int			y;
 	int			x;
 	t_vector	cam_dir;
-	t_color		color;
+	int			color;
+
+    clock_t start, end;
+    double cpu_time_used;
+	start = clock();
 
 	make_cam_matrix(&obj->camera);
 	cam_dir = set_vector(0.0, 0.0, 0.0);
@@ -153,9 +187,12 @@ void	render(t_rt *obj)
 		{
 			make_camera_ray(obj, x, y, &cam_dir);
 			color = trace_ray(&obj->camera.pos, &cam_dir, obj);
-			my_pixel_put(&obj->mlx, x, y, convert_color(color));
+			my_pixel_put(&obj->mlx, x, y, color);
 			x++;
 		}
 		y++;
 	}
+	end = clock();
+	cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("Time taken by function: %f seconds\n", cpu_time_used);
 }
